16b4381e0cf3ea03d17d6d6f2320652834b93d29
Aayush Yadav <aayush130405@gmail.com>
2026-07-20 00:04:18 +0800

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        lowered = s.lower()
+        alnum_str = []
+
+        for i in lowered:
+            if i.isalnum():
+                alnum_str.append(i)
+
+        left = 0
+        right = len(alnum_str) - 1
+
+        while left < right:
+            if alnum_str[left] == alnum_str[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+
+        return True
