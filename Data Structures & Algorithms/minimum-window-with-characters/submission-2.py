9e8efb46fe54776a22808e4d35ec79f205284ffd
Aayush Yadav <aayush130405@gmail.com>
2026-08-21 14:37:07 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        freq = {}
+        win_map = {}
+
+        l = 0
+
+        minn = float("inf")
+
+        best_left = 0
+        best_right = len(s)
+
+        for i in t:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] += 1
+        
+        cond = len(freq) #3
+
+        for r in range(len(s)):
+            if s[r] not in win_map:
+                win_map[s[r]] = 1
+                if s[r] in freq and win_map[s[r]] == freq[s[r]]:
+                    cond -= 1
+            else:
+                win_map[s[r]] += 1
+                if s[r] in freq and win_map[s[r]] == freq[s[r]]:
+                    cond -= 1
+            
+            while cond == 0:
+                if r - l + 1 < minn:
+                    minn = r - l + 1
+                    best_left = l
+                    best_right = r
+                win_map[s[l]] -= 1
+                if s[l] in freq and win_map[s[l]] < freq[s[l]]:
+                    cond += 1
+                l += 1
+
+        if minn == float("inf"):
+            return ""
+        else:
+            return s[best_left:best_right + 1]
